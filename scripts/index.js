c7dd3f95fca07777e3cc484ef54872d010977882
pilar <sauleturme@gmail.com>
2018-06-13 16:47:33 +0200

    added new method to resolve the exercises in js anda some styles

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,37 @@
 
-let divOneSelector = document.querySelector('#dice-1')
-let divTwoSelector = document.querySelector('#dice-2')
-let button = document.querySelector('.button')
+function getRandom (min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
 
 let divOne = {
-  value: () => Math.floor(Math.random() * 6 + 1),
+  value: 1,
   roll: function () {
-    return divOneSelector.className = `dice_${divOne.value()}`
+    this.value = getRandom(1, 6)
+    return this.value
   }
 }
-
 let divTwo = {
-  value: () => Math.floor(Math.random() * 6 + 1),
+  value: 1,
   roll: function () {
-    return divTwoSelector.className = `dice_${divTwo.value()}`
+    this.value = getRandom(1, 6)
+    return this.value
   }
 }
 
+function printDices (one, two) {
+
+  let divOneSelector = document.querySelector('#dice-1');
+  let divTwoSelector = document.querySelector('#dice-2');
+
+  divOneSelector.style['background'] = `url(/images/inverted-dice-${one}.svg) no-repeat`
+  divTwoSelector.style['background'] = `url(/images/inverted-dice-${two}.svg) no-repeat`
+}
+
+let button = document.querySelector('.button')
 button.addEventListener('click', function () {
-  divOne.roll()
-  divTwo.roll()
+  printDices(divOne.roll(), divTwo.roll())
 })
+
+printDices(divOne.value, divTwo.value)
